8e4beb9525d122954922662b6b317362e80cce32
matovu-farid <matovu90@gmail.com>
2022-01-17 11:41:37 +0300

    Remove unused code

--- src/add_remove_edit.js
+++
@@ -1,7 +0,0 @@
-import { listManager } from './list_manager';
-
-export const editTask = (task) => listManager.edit(task);
-export const removeTask = (task) => listManager.remove(task);
-export const addTask = (taskInput) => listManager.addTask(taskInput);
-export const clearTasks = () => listManager.clearCompleted();
-export const getArray = () => listManager.array;
--- src/clear_completed.js
+++
@@ -1,7 +0,0 @@
-import { listManager } from './list_manager';
-
-const clearCompleted = () => {
-  listManager.clearCompleted();
-};
-
-export { clearCompleted as default };
